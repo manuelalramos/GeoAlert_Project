5e3518b4cd777b25e9922890a992d184d8b71da0
manuelalramos <manuela.lima.r@outlook.com>
2026-06-06 15:35:59 -0300

    feat, fix and style : Adciona interatividade dos blocos de pergunta. Corrigi html do faq. Estiliza página faq.

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// FAQ
+
+const faq1 = document.querySelector("#faq1");
+const faq2 = document.querySelector("#faq2");
+const faq3 = document.querySelector("#faq3");
+const faq4 = document.querySelector("#faq4");
+
+function abrirFecharFaq(item){
+    item.classList.toggle("active");
+}
+
+if (faq1) {
+  faq1.addEventListener("click", function () {
+    abrirFecharFaq(faq1);
+  });
+}
+
+if (faq2) {
+  faq2.addEventListener("click", function () {
+    abrirFecharFaq(faq2);
+  });
+}
+
+if (faq3) {
+  faq3.addEventListener("click", function () {
+    abrirFecharFaq(faq3);
+  });
+}
+
+if (faq4) {
+  faq4.addEventListener("click", function () {
+    abrirFecharFaq(faq4);
+  });
+}
+
+
